f0108598392f6a14a4f9fa6fc56a2e5d913fdfa9
terahn <terahnharrison@gmail.com>
2020-07-24 16:27:02 -0700

    cleanup old code

--- client/src/App.js
+++ client/src/App.js
@@ -37,31 +37,6 @@ import logo from './statify-logo.png';
 
 const spotifyWebApi = new Spotify();
 
-const genreKeywords = [
-  ['pop'],
-  ['country'],
-  ['classical', 'soundtrack'],
-  [
-    'bass',
-    'chillwave',
-    'future',
-    'tronic',
-    'house',
-    'rave',
-    'dub',
-    'dance',
-    'trap',
-    'vapor',
-    'techno',
-    'edm',
-  ],
-  ['indie'],
-  ['rock', 'punk', 'metal', 'grunge'],
-  ['jazz', 'bop', 'swing', 'stride'],
-  ['r&b', 'urban', 'soul', 'funk'],
-  ['hip hop', 'rap'],
-];
-
 const cardSlide = {
   initial: { x: -100, opacity: 0 },
   animate: { x: 0, opacity: 1 },
@@ -81,30 +56,8 @@ class App extends Component {
       displayContent: false,
       hasAccessToken,
       userFirstname: '',
-      topArtists: {
-        names: [],
-        ids: [],
-        albumImgs: [],
-        genres: [],
-      },
-      topTracks: {
-        names: [],
-        artist: [],
-        ids: [],
-        albumImgs: [],
-      },
-      genreData: [],
-      genreLabels: [
-        'Pop',
-        'Country',
-        'Classical',
-        'Electronic',
-        'Indie',
-        'Rock',
-        'Jazz',
-        'R&B/Soul',
-        'Hip-Hop',
-      ],
+      currentArtists: [],
+      topTracks: [],
       graphData: {
         nodes: [],
         links: [],
@@ -128,21 +81,19 @@ class App extends Component {
       });
     }
     const name = await this.getUserInfo();
-    const { topTracks } = await this.getTopTracks();
-    const {
-      topArtists,
-      genreData,
-      displayContent,
-    } = await this.getTopArtists();
+    // const { topTracks } = await this.getTopTracks();
+    const { topArtists } = await this.getTopArtists();
     const graphNodes = await this.buildGraphNodes(topArtists);
-    const graphLinks = await this.buildGraphLinks(topArtists);
+    const [graphLinks, relatedArtists] = await this.buildGraphLinks(topArtists);
+    topArtists.forEach((artist, index) => {
+      artist.relatedArtists = relatedArtists[index].artists;
+    });
     this.setState((prevState) => ({
       ...prevState,
-      displayContent,
+      displayContent: true,
       userFirstname: name[0],
-      topArtists,
-      topTracks,
-      genreData,
+      currentArtists: topArtists,
+      // topTracks,
       graphData: {
         // nodes: this.decorateGraphNodesWithInitialPositioning(graphNodes),
         nodes: graphNodes,
@@ -183,12 +134,6 @@ class App extends Component {
   };
 
   getTopArtists = () => {
-    const nameArray = [];
-    const idArray = [];
-    const albumImgArray = [];
-    const genreArray = [];
-    const genreData = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let currGenreData = [];
     const artistLimit = 50;
     const { timeRange } = this.state;
 
@@ -199,31 +144,8 @@ class App extends Component {
           time_range: timeRange,
         })
         .then((data) => {
-          for (let i = 0; i < artistLimit; i += 1) {
-            if (data.items[i] !== undefined) {
-              nameArray.push(data.items[i].name);
-              idArray.push(data.items[i].id);
-              albumImgArray.push(data.items[i].images[0].url);
-              genreArray.push(data.items[i].genres);
-              currGenreData = this.genreChartMaker(data.items[i].genres);
-              for (let j = 0; j < 9; j += 1) {
-                // update genre data with new data plus a weight depending on how high up on the top artists list they come from
-                if (currGenreData[j] !== 0) {
-                  genreData[j] += currGenreData[j] + (25 - i) / 4;
-                }
-              }
-            }
-          }
-
           resolve({
-            topArtists: {
-              names: nameArray,
-              ids: idArray,
-              albumImgs: albumImgArray,
-              topGenres: genreArray,
-            },
-            genreData,
-            displayContent: true,
+            topArtists: data.items,
           });
         })
         .catch((error) => reject(error));
@@ -231,53 +153,17 @@ class App extends Component {
   };
 
   getTopTracks = () => {
-    const nameArray = [];
-    const artistArray = [];
-    const idArray = [];
-    const albumImgArray = [];
     const { timeRange } = this.state;
     return new Promise((resolve, reject) => {
       spotifyWebApi
         .getMyTopTracks({ limit: 50, time_range: timeRange })
         .then((data) => {
-          for (let i = 0; i < 50; i += 1) {
-            if (data.items[i] !== undefined) {
-              nameArray.push(data.items[i].name);
-              artistArray.push(data.items[i].artists[0].name);
-              idArray.push(data.items[i].id);
-              albumImgArray.push(data.items[i].album.images[0].url);
-            }
-          }
-
           resolve({
-            topTracks: {
-              names: nameArray,
-              artist: artistArray,
-              ids: idArray,
-              albumImgs: albumImgArray,
-            },
+            topTracks: data.items,
           });
         })
         .catch((error) => reject(error));
     });
-  };
-
-  genreChartMaker = (genres) => {
-    // console.log(genres)
-    const genreCheckbox = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    for (let i = 0; i < genres.length; i += 1) {
-      // check each genre against the genre keywords. Tick the genreCheckBox if applicable. Once done, update state's genre data
-      for (let j = 0; j < 9; j += 1) {
-        if (genreCheckbox[j] !== 1) {
-          for (let k = 0; k < genreKeywords[j].length; k += 1) {
-            if (genres[i].indexOf(genreKeywords[j][k]) !== -1) {
-              genreCheckbox[j] = 1;
-            }
-          }
-        }
-      }
-    }
-    return genreCheckbox;
   };
 
   changeTerm = (event) => {
@@ -292,29 +178,13 @@ class App extends Component {
     }
   };
 
-  renderGenreData = () => {
-    const { genreData, genreLabels } = this.state;
-    const indexed = genreData.map((x, idx) => {
-      return { label: genreLabels[idx], val: x };
-    });
-    indexed.sort((x, y) => {
-      return x.val < y.val ? 1 : x.val === y.val ? 0 : -1;
-    });
-    const top = indexed.slice(0, 5);
-
-    return top.map((x, idx) => {
-      return <Genre label={x.label} value={x.val} key={`genre_${x.label}`} />;
-    });
-  };
-
-  buildGraphNodes = ({ ids, names, albumImgs }) => {
+  buildGraphNodes = (artists) => {
     return new Promise((resolve) => {
       const graphNodes = [];
-      ids.forEach((x, index) => {
+      artists.forEach((artist) => {
         graphNodes.push({
-          id: names[index],
-          // svg: albumImgs[index],
-          img: albumImgs[index],
+          id: artist.name,
+          img: artist.images[2].url,
         });
       });
 
@@ -322,32 +192,34 @@ class App extends Component {
     });
   };
 
-  buildGraphLinks = ({ ids, names }) => {
+  buildGraphLinks = (artists, fakeLinks = true) => {
     return Promise.all(
-      ids.map((element) => spotifyWebApi.getArtistRelatedArtists(element))
+      artists.map((artist) => spotifyWebApi.getArtistRelatedArtists(artist.id))
     )
       .then((data) => {
+        const relatedArtists = data;
         let links = [];
         data.forEach((element, srcIdx) => {
           let artistLinks = [];
           element.artists.forEach((x) => {
-            const artistIndex = ids.indexOf(x.id);
+            const found = artists.find((artist) => artist.id === x.id);
             // there is a valid link
-            if (artistIndex > 0) {
+            if (found) {
               artistLinks.push({
-                source: names[srcIdx],
-                target: names[artistIndex],
+                source: artists[srcIdx].name,
+                target: found.name,
                 linkType: true,
               });
             }
           });
 
           // add a fake link (temporary hack in order for unlinked nodes to not be bunched up in the top left corner)
-          if (artistLinks.length === 0) {
+          if (artistLinks.length === 0 && fakeLinks) {
             artistLinks = [
               {
-                source: names[srcIdx],
-                target: names[Math.floor(Math.random() * names.length)],
+                source: artists[srcIdx].name,
+                target:
+                  artists[Math.floor(Math.random() * artists.length)].name,
                 opacity: 0.01,
                 linkType: false,
               },
@@ -357,7 +229,7 @@ class App extends Component {
           links = [...links, ...artistLinks];
         });
 
-        return links;
+        return [links, relatedArtists];
       })
       .catch((error) => error);
   };
@@ -406,10 +278,11 @@ class App extends Component {
   };
 
   renderSelectedCard = (nodeId) => {
-    const { graphData, topArtists } = this.state;
+    const { graphData, currentArtists } = this.state;
     try {
-      const artistImg = graphData.nodes.find((node) => node.id === nodeId).img;
-      const artistId = 'PUT SOMETHING HERE';
+      const artist = currentArtists.find((x) => x.name === nodeId);
+      const artistUrl = artist.external_urls.spotify;
+      const artistImg = artist.images[0].url;
       const connections = [];
       graphData.links.forEach((link) => {
         if (link.source === nodeId) {
@@ -447,14 +320,18 @@ class App extends Component {
               <Button
                 size="small"
                 color="primary"
-                href={`https://open.spotify.com/artist/${artistId}`}
+                href={artistUrl}
                 target="_blank"
               >
                 View On Spotify
               </Button>
-              <Button size="small" color="primary">
+              {/* <Button
+                size="small"
+                color="primary"
+                onClick={() => this.viewRelatedArtists(artist)}
+              >
                 View All Related Artists
-              </Button>
+              </Button> */}
             </CardActions>
           </Card>
         </motion.div>
@@ -463,6 +340,31 @@ class App extends Component {
       console.warn(e);
       return null;
     }
+  };
+
+  viewRelatedArtists = async (selectedArtist) => {
+    const { currentArtists } = this.state;
+    const artists = selectedArtist.relatedArtists.concat(currentArtists);
+    const graphNodes = await this.buildGraphNodes(artists);
+    const [graphLinks, relatedArtists] = await this.buildGraphLinks(
+      artists,
+      false
+    );
+    artists.forEach((artist, index) => {
+      artist.relatedArtists = relatedArtists[index].artists;
+    });
+    const filteredLinks = graphLinks.filter(
+      (link) =>
+        currentArtists.find((artist) => artist.name === link.source) ||
+        currentArtists.find((artist) => artist.name === link.target)
+    );
+    this.setState({
+      graphData: {
+        nodes: graphNodes,
+        links: Array.from(new Set(filteredLinks)),
+      },
+      currentArtists: artists,
+    });
   };
 
   openModal = () => {
